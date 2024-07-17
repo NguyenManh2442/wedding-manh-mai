ce113e52d87d8ad11d19b96b1e2fddb1bcaeab58
nguyenmanh2442 <nguyenmanh25111999m@gmail.com>
2024-07-17 21:54:05 +0700

    update image

--- src/common/components/Gallery/index.tsx
+++ src/common/components/Gallery/index.tsx
@@ -6,72 +6,100 @@ import * as CS from 'src/common/components/Styles';
 function Gallery() {
   const images = [
     {
-      original: '/images/mobile/SYD_0013_padding.png',
-      thumbnail: '/images/mobile/SYD_0013_padding.png',
+      original: '/images/mobile/VVT01244.jpg',
+      thumbnail: '/images/mobile/VVT01244.jpg',
     },
     {
-      original: '/images/mobile/SYD_0056_padding.png',
-      thumbnail: '/images/mobile/SYD_0056_padding.png',
+      original: '/images/mobile/VVT01098.jpg',
+      thumbnail: '/images/mobile/VVT01098.jpg',
     },
     {
-      original: '/images/mobile/SYD_0140.jpg',
-      thumbnail: '/images/mobile/SYD_0140.jpg',
+      original: '/images/mobile/VVT01127.jpg',
+      thumbnail: '/images/mobile/VVT01127.jpg',
     },
     {
-      original: '/images/mobile/SYD_0237_padding.png',
-      thumbnail: '/images/mobile/SYD_0237_padding.png',
+      original: '/images/mobile/VVT01272.jpg',
+      thumbnail: '/images/mobile/VVT01272.jpg',
     },
     {
-      original: '/images/mobile/SYD_0270_padding.png',
-      thumbnail: '/images/mobile/SYD_0270_padding.png',
+      original: '/images/mobile/VVT01269.jpg',
+      thumbnail: '/images/mobile/VVT01269.jpg',
     },
     {
-      original: '/images/studio/SYD_0305.jpg',
-      thumbnail: '/images/studio/SYD_0305.jpg',
+      original: '/images/mobile/VVT00360.jpg',
+      thumbnail: '/images/mobile/VVT00360.jpg',
     },
     {
-      original: '/images/mobile/SYD_0328.jpg',
-      thumbnail: '/images/mobile/SYD_0328.jpg',
+      original: '/images/mobile/VVT00420.jpg',
+      thumbnail: '/images/mobile/VVT00420.jpg',
     },
     {
-      original: '/images/mobile/SYD_0357.jpg',
-      thumbnail: '/images/mobile/SYD_0357.jpg',
+      original: '/images/mobile/VVT00443.jpg',
+      thumbnail: '/images/mobile/VVT00443.jpg',
     },
     {
-      original: '/images/studio/SYD_0487_padding.png',
-      thumbnail: '/images/studio/SYD_0487_padding.png',
+      original: '/images/mobile/VVT00469.jpg',
+      thumbnail: '/images/mobile/VVT00469.jpg',
     },
     {
-      original: '/images/studio/SYD_0490_padding.png',
-      thumbnail: '/images/studio/SYD_0490_padding.png',
+      original: '/images/mobile/VVT00554.jpg',
+      thumbnail: '/images/mobile/VVT00554.jpg',
     },
     {
-      original: '/images/mobile/SYD_0508.jpg',
-      thumbnail: '/images/mobile/SYD_0508.jpg',
+      original: '/images/mobile/VVT00585.jpg',
+      thumbnail: '/images/mobile/VVT00585.jpg',
     },
     {
-      original: '/images/studio/SYD_0553.jpg',
-      thumbnail: '/images/studio/SYD_0553.jpg',
+      original: '/images/mobile/VVT00616.jpg',
+      thumbnail: '/images/mobile/VVT00616.jpg',
     },
     {
-      original: '/images/studio/SYD_0570.jpg',
-      thumbnail: '/images/studio/SYD_0570.jpg',
+      original: '/images/mobile/VVT00691.jpg',
+      thumbnail: '/images/mobile/VVT00691.jpg',
     },
     {
-      original: '/images/mobile/SYD_0602.jpg',
-      thumbnail: '/images/mobile/SYD_0602.jpg',
+      original: '/images/mobile/VVT00855.jpg',
+      thumbnail: '/images/mobile/VVT00855.jpg',
     },
     {
-      original: '/images/studio/SYD_0613.jpg',
-      thumbnail: '/images/studio/SYD_0613.jpg',
+      original: '/images/mobile/VVT00327.jpg',
+      thumbnail: '/images/mobile/VVT00327.jpg',
     },
     {
-      original: '/images/studio/SYD_0729_padding.png',
-      thumbnail: '/images/studio/SYD_0729_padding.png',
+      original: '/images/mobile/VVT00002.jpg',
+      thumbnail: '/images/mobile/VVT00002.jpg',
     },
     {
-      original: '/images/studio/SYD_0740_padding.png',
-      thumbnail: '/images/studio/SYD_0740_padding.png',
+      original: '/images/mobile/VVT00019.jpg',
+      thumbnail: '/images/mobile/VVT00019.jpg',
+    },
+    {
+      original: '/images/mobile/VVT00024.jpg',
+      thumbnail: '/images/mobile/VVT00024.jpg',
+    },
+    {
+      original: '/images/mobile/VVT00054.jpg',
+      thumbnail: '/images/mobile/VVT00054.jpg',
+    },
+    {
+      original: '/images/mobile/VVT00093.jpg',
+      thumbnail: '/images/mobile/VVT00093.jpg',
+    },
+    {
+      original: '/images/mobile/VVT00221.jpg',
+      thumbnail: '/images/mobile/VVT00221.jpg',
+    },
+    {
+      original: '/images/mobile/VVT00168.jpg',
+      thumbnail: '/images/mobile/VVT00168.jpg',
+    },
+    {
+      original: '/images/mobile/VVT00336.jpg',
+      thumbnail: '/images/mobile/VVT00336.jpg',
+    },
+    {
+      original: '/images/mobile/VVT00144.jpg',
+      thumbnail: '/images/mobile/VVT00144.jpg',
     },
   ];
 
